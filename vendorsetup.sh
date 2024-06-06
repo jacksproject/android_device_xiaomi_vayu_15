5040d05e12e57124183d1043924bceb04732521f
GXC2356 <gxcdev2356@gmail.com>
2024-06-06 14:35:05 +0300

    vayu: vendorsetup.sh: Remove sources before clone

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,11 @@
 # Vendor
+rm -rf vendor/xiaomi/vayu
 git clone --depth=1 https://github.com/ProjectBlaze-Devices/vendor_xiaomi_vayu.git vendor/xiaomi/vayu
 
 # Kernel (Skyline)
+rm -rf kernel/xiaomi/vayu
 git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
 
 # Dolby
+rm -rf vendor/dolby
 git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
