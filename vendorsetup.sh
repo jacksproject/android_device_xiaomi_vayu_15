546d1cb43f3014d398a47026fbd91fa5ef45d0bb
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2024-09-18 22:46:55 +0300

    vendorsetup.sh: Remove Dolby

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,6 @@ git clone --depth=1 https://github.com/ProjectBlaze-Devices/vendor_xiaomi_vayu.g
 rm -rf kernel/xiaomi/vayu
 git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
 
-# Dolby
-rm -rf vendor/dolby
-git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
-
 # Hardware
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
